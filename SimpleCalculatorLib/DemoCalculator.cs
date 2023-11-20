5c99052dc802a24c1ba687d466e15827efa1dfcb
DKriukov777 <DKriukov@eagle.org>
2023-11-20 12:45:39 -0600

    FirstCommit

---
+++ SimpleCalculatorLib/DemoCalculator.cs
@@ -0,0 +1,17 @@
+﻿namespace SimpleCalculatorLib;
+
+public class DemoCalculator
+{
+    
+    public int Plus(int a, int b)
+    {
+        return a + b;
+    }
+
+   
+    public int Minus(int a, int b)
+    {
+        return a - b;
+    }
+    
+}
